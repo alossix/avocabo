38b67b5e3adfe6bc26f82fb0d9979d1b542f37b5
Rich Alossi <alossix@users.noreply.github.com>
2023-04-28 11:39:35 +0200

    feat: update profile page view (#60)

--- src/components/PageViews/ProfilePageView/ProfilePageView.test.tsx
+++ src/components/PageViews/ProfilePageView/ProfilePageView.test.tsx
@@ -1,81 +1,89 @@
 import { mockVocabList } from "@/lib/testUtils";
-import { useAppSelector } from "@/store/hooks";
-import { RootState, store } from "@/store/store";
+import { AppUser, LearningLanguages } from "@/types/general";
 import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
 import { ProfilePageView } from "./ProfilePageView";
 
-jest.mock("@/store/hooks");
+const currentUser: AppUser = {
+  email: "test@example.com",
+  emailVerified: true,
+  displayName: "MyName",
+  uid: "12345-abcde-67890-fghij",
+  interfaceLanguage: "en",
+  learningLanguage: "ca",
+  userCreatedDate: new Date("2023-01-01"),
+  userLastSignIn: new Date("2023-01-15"),
+};
 
-const mockedUseAppSelector = useAppSelector as jest.MockedFunction<
-  typeof useAppSelector
->;
-
-const initialState: RootState = {
-  auth: {
-    user: {
-      email: "test@example.com",
-      emailVerified: true,
-      displayName: "MyName",
-      uid: "12345-abcde-67890-fghij",
-      interfaceLanguage: "en",
-      learningLanguage: "ca",
-      userCreatedDate: new Date("2023-01-01"),
-      userLastSignIn: new Date("2023-01-15"),
-    },
-    loading: false,
-    error: "",
-  },
-  interfaceLanguage: {
-    interfaceLanguage: "en",
-  },
-  vocab: mockVocabList,
+const languageLabelsLearning = {
+  ca: "Català",
+  en: "English",
+  es: "Español",
+  de: "Deutsch",
+  fr: "Français",
+  it: "Italiano",
+  nl: "Nederlands",
+  uk: "Українська",
+  other: "common:other",
 };
 
 describe("ProfilePageView", () => {
-  beforeEach(() => {
-    store.dispatch({ type: "SET_INITIAL_STATE", payload: initialState });
-
-    // Mock the useAppSelector to return the initial state
-    mockedUseAppSelector.mockImplementation((selector) =>
-      selector(initialState)
-    );
-  });
-
-  afterEach(() => {
-    // Clear the mock implementation after each test
-    mockedUseAppSelector.mockClear();
-  });
-
-  it("should render the correct number of vocab items", () => {
+  it("should render the user's display name", () => {
     render(
-      <Provider store={store}>
-        <ProfilePageView vocabList={mockVocabList} />
-      </Provider>
+      <ProfilePageView
+        currentUser={currentUser}
+        vocabCount={mockVocabList.length}
+      />
     );
+    expect(screen.getByText(currentUser.displayName ?? "")).toBeInTheDocument();
+  });
 
-    const vocabItems = screen.getAllByText(/(chair|desk|door)/);
-    expect(vocabItems.length).toBe(mockVocabList.length);
+  it("should render the user's email", () => {
+    render(
+      <ProfilePageView
+        currentUser={currentUser}
+        vocabCount={mockVocabList.length}
+      />
+    );
+    expect(screen.getByText(currentUser.email ?? "")).toBeInTheDocument();
   });
 
   it("should render the total number of vocab items", () => {
     render(
-      <Provider store={store}>
-        <ProfilePageView vocabList={mockVocabList} />
-      </Provider>
+      <ProfilePageView
+        currentUser={currentUser}
+        vocabCount={mockVocabList.length}
+      />
     );
-
     expect(
       screen.getByText(mockVocabList.length.toString())
     ).toBeInTheDocument();
   });
 
-  it("should render the user's email", () => {
-    render(<ProfilePageView vocabList={mockVocabList} />);
-    if (initialState.auth.user) {
-      expect(
-        screen.getByText(initialState.auth.user.email as string)
-      ).toBeInTheDocument();
-    }
+  it("should render the user's learning language", () => {
+    render(
+      <ProfilePageView
+        currentUser={currentUser}
+        vocabCount={mockVocabList.length}
+      />
+    );
+    const learningLanguage = "Català";
+    expect(screen.getByText(learningLanguage)).toBeInTheDocument();
   });
+
+  it.each(Object.entries(languageLabelsLearning))(
+    "should render the correct learning language name for key %s",
+    (learningLanguageKey, expectedLanguageName) => {
+      const testUser: AppUser = {
+        ...currentUser,
+        learningLanguage: learningLanguageKey as LearningLanguages,
+      };
+      render(
+        <ProfilePageView
+          currentUser={testUser}
+          vocabCount={mockVocabList.length}
+        />
+      );
+      expect(screen.getByText(expectedLanguageName)).toBeInTheDocument();
+    }
+  );
 });
--- src/components/PageViews/ProfilePageView/ProfilePageView.tsx
+++ src/components/PageViews/ProfilePageView/ProfilePageView.tsx
@@ -1,26 +1,55 @@
-import { useAppSelector } from "@/store/hooks";
-import { selectUserSignedIn } from "@/store/slices/authSlice";
-import { Vocab } from "@/types/vocab";
+import { AppUser, LearningLanguages } from "@/types/general";
+import styled from "@emotion/styled";
 import useTranslation from "next-translate/useTranslation";
 
 type ProfilePageViewProps = {
-  vocabList: Vocab[];
+  currentUser: AppUser;
+  vocabCount: number;
 };
 
 export const ProfilePageView: React.FC<ProfilePageViewProps> = ({
-  vocabList,
+  currentUser,
+  vocabCount,
 }) => {
   const { t } = useTranslation("common");
-  const currentUser = useAppSelector(selectUserSignedIn);
+  const languageLabelsLearning: Record<LearningLanguages, string> = {
+    ca: "Català",
+    en: "English",
+    es: "Español",
+    de: "Deutsch",
+    fr: "Français",
+    it: "Italiano",
+    nl: "Nederlands",
+    uk: "Українська",
+    other: `${t("common:other")}`,
+  };
+  const languageName = languageLabelsLearning[currentUser.learningLanguage];
 
   return (
-    <div>
+    <ProfilePageContainer>
       <h1>{t("common:header_profile")}</h1>
-      {vocabList.map((word) => (
-        <p key={word.vocabId}>{`${word.vocabId} ${word.definition}`}</p>
-      ))}
-      <p>{vocabList.length}</p>
-      <p>{currentUser?.email}</p>
-    </div>
+      <div>
+        <h3>{t("common:profile_name")}</h3>
+        <p>{currentUser.displayName}</p>
+      </div>
+      <div>
+        <h3>{t("common:email")}</h3>
+        <p>{currentUser.email}</p>
+      </div>
+      <div>
+        <h3>{t("common:number_of_vocab_entries")}</h3>
+        <p>{vocabCount}</p>
+      </div>
+      <div>
+        <h3>{t("common:learning_language_simple")}</h3>
+        <p>{languageName}</p>
+      </div>
+    </ProfilePageContainer>
   );
 };
+
+const ProfilePageContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  gap: 8,
+});
